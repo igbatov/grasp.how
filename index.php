97c4fc502575202934ff40fbf8a49758f6faf597
igbatov <igbatov@telediscount.ru>
2015-05-27 19:44:45 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "yovalue_tmp";
+$password = "123";
+$dbname = "yovalue";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id FROM auth";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: ". "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
