b740c5716d89fa3b598dc9c37e74e08cb9f6be29
Igor Batov <igbatov@gmail.com>
2016-12-08 05:06:17 -0500

    server

--- scripts/graphImageGenerator/converter.js
+++ scripts/graphImageGenerator/converter.js
@@ -1,27 +1,63 @@
 var fs = require('fs');
 var d3 = require('d3');
 var jsdom = require('jsdom');
-var mysql      = require('mysql');
 
 var arg = process.argv[2];
 
 console.log(arg);
 
+var chartWidth = 500, chartHeight = 500;
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'yovalue',
-  password : '',
-  database : 'yovalue'
-});
+var arc = d3.svg.arc()
+    .outerRadius(chartWidth/2 - 10)
+    .innerRadius(0);
 
-connection.connect();
+var colours = ['#F00','#000','#000','#000','#000','#000','#000','#000','#000'];
 
-connection.query('SELECT * from graph LIMIT 2', function(err, rows, fields) {
-  if (!err)
-    console.log('The solution is: ', rows);
-  else
-    console.log('Error while performing Query.');
-});
+module.exports = function( pieData, outputLocation ){
+	if(!pieData) pieData = [12,31];
+	if(!outputLocation) outputLocation = 'test.svg';
 
-connection.end();
+	jsdom.env({
+	    html:'',
+	    features:{ QuerySelector:true }, //you need query selector for D3 to work
+	    done:function(errors, window){
+	    	window.d3 = d3.select(window.document); //get d3 into the dom
+
+	    	//do yr normal d3 stuff
+	    	var svg = window.d3.select('body')
+	    		.append('div').attr('class','container') //make a container div to ease the saving process
+	    		.append('svg')
+	    			.attr({
+			      		xmlns:'http://www.w3.org/2000/svg',
+			      		width:chartWidth,
+			      		height:chartHeight
+			      	})
+			    .append('g')
+			    	.attr('transform','translate(' + chartWidth/2 + ',' + chartWidth/2 + ')');
+
+	    	svg.selectAll('.arc')
+	    		.data( d3.layout.pie()(pieData) )
+	    			.enter()
+	    		.append('path')
+	    			.attr({
+	    				'class':'arc',
+	    				'd':arc,
+	    				'fill':function(d,i){
+	    					return colours[i];
+	    				},
+	    				'stroke':'#fff'
+	    			});
+
+	    	//write out the children of the container div
+    		fs.writeFileSync(outputLocation, window.d3.select('.container').html()) //using sync to keep the code simple
+
+	    }
+	});
+}
+
+if (require.main === module) {
+    module.exports();
+}
+
+
