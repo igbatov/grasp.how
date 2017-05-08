9a96e8642a163d9b6499787463e6e425e052f4d9
Igor Batov <igbatov@gmail.com>
2017-05-08 23:50:36 +0300

    modified upload.php to upload in backup directory

--- scripts/google/upload.php
+++ scripts/google/upload.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
-//var_dump($argv); exit();
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/uploadlib.php';
 
 define('APPLICATION_NAME', 'Drive API PHP uploader');
 define('CREDENTIALS_PATH', '~/.credentials/drive-php-uploader.json');
@@ -11,71 +11,10 @@ define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
 define('SCOPES', implode(' ', array(
   Google_Service_Drive::DRIVE_FILE)
 ));
+define('GENERAL_UPLOAD_DIR_NAME', 'GRASP_BACKUP');
 
 if (php_sapi_name() != 'cli') {
   throw new Exception('This application must be run on the command line.');
-}
-
-/**
- * Returns an authorized API client.
- * @return Google_Client the authorized client object
- */
-function getClient() {
-  $client = new Google_Client();
-  $client->setApplicationName(APPLICATION_NAME);
-  $client->setScopes(SCOPES);
-  $client->setAuthConfigFile(CLIENT_SECRET_PATH);
-  $client->setAccessType('offline');
-  $client->setRedirectUri('http://grasp.how/oauth2callback');
-
-  // Load previously authorized credentials from a file.
-  $credentialsPath = expandHomeDirectory(CREDENTIALS_PATH);
-  if (file_exists($credentialsPath)) {
-    $accessToken = file_get_contents($credentialsPath);
-  } else {
-    // Request authorization from the user.
-    $authUrl = $client->createAuthUrl();
-    printf("Open the following link in your browser:\n%s\n", $authUrl);
-    print 'Enter verification code: ';
-    $authCode = trim(fgets(STDIN));
-
-    // Exchange authorization code for an access token.
-    $accessToken = $client->authenticate($authCode);
-
-    // Store the credentials to disk.
-    if(!file_exists(dirname($credentialsPath))) {
-      mkdir(dirname($credentialsPath), 0700, true);
-    }
-    file_put_contents($credentialsPath, $accessToken);
-    printf("Credentials saved to %s\n", $credentialsPath);
-  }
-  $client->setAccessToken($accessToken);
-
-  // Refresh the token if it's expired.
-  if ($client->isAccessTokenExpired()) {
-    $client->refreshToken($client->getRefreshToken());
-    file_put_contents($credentialsPath, $client->getAccessToken());
-  }
-  return $client;
-}
-
-/**
- * Expands the home directory alias '~' to the full path.
- * @param string $path the path to expand.
- * @return string the expanded path.
- */
-function expandHomeDirectory($path) {
-  $homeDirectory = getenv('HOME');
-  if (empty($homeDirectory)) {
-    $homeDirectory = getenv("HOMEDRIVE") . getenv("HOMEPATH");
-  }
-  return str_replace('~', realpath($homeDirectory), $path);
-}
-
-function deleteOldest($service){
- $files = $service->files->listFiles(array('orderBy'=>'createdTime asc'));
- echo 'removed '.$files->getFiles()[0]['name'].' (id='.$files->getFiles()[0]['id'].")\n";
- $service->files->delete($files->getFiles()[0]['id']);
 }
 
 // Get the API client and construct the service object.
@@ -83,6 +22,9 @@ $client = getClient();
 $service = new Google_Service_Drive($client);
 $filepath = $argv[1];
 $data = file_get_contents($filepath);
+$fileName = basename($filepath);
+
+$generalUploadDirId = getFolderExistsCreate($service, GENERAL_UPLOAD_DIR_NAME);
 
 // create and upload a new Google Drive file, including the data
 try
@@ -90,11 +32,12 @@ try
 	//Insert a file
 	$file = new Google_Service_Drive_DriveFile($client);
 
-	$file->setName(basename($filepath));
+	$file->setName($fileName);
 
 	$createdFile = $service->files->create($file, array(
 		'data' => $data,
 		'uploadType' => 'media',
+    'parents' => array($generalUploadDirId)
 	));
 
 //	deleteOldest($service);
---
+++ scripts/google/uploadlib.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient() {
+  $client = new Google_Client();
+  $client->setApplicationName(APPLICATION_NAME);
+  $client->setScopes(SCOPES);
+  $client->setAuthConfigFile(CLIENT_SECRET_PATH);
+  $client->setAccessType('offline');
+  $client->setRedirectUri('http://grasp.how/oauth2callback');
+
+  // Load previously authorized credentials from a file.
+  $credentialsPath = expandHomeDirectory(CREDENTIALS_PATH);
+  if (file_exists($credentialsPath)) {
+    $accessToken = file_get_contents($credentialsPath);
+  } else {
+    // Request authorization from the user.
+    $authUrl = $client->createAuthUrl();
+    printf("Open the following link in your browser:\n%s\n", $authUrl);
+    print 'Enter verification code: ';
+    $authCode = trim(fgets(STDIN));
+
+    // Exchange authorization code for an access token.
+    $accessToken = $client->authenticate($authCode);
+
+    // Store the credentials to disk.
+    if(!file_exists(dirname($credentialsPath))) {
+      mkdir(dirname($credentialsPath), 0700, true);
+    }
+    file_put_contents($credentialsPath, $accessToken);
+    printf("Credentials saved to %s\n", $credentialsPath);
+  }
+  $client->setAccessToken($accessToken);
+
+  // Refresh the token if it's expired.
+  if ($client->isAccessTokenExpired()) {
+    $client->refreshToken($client->getRefreshToken());
+    file_put_contents($credentialsPath, $client->getAccessToken());
+  }
+  return $client;
+}
+
+/**
+ * Expands the home directory alias '~' to the full path.
+ * @param string $path the path to expand.
+ * @return string the expanded path.
+ */
+function expandHomeDirectory($path) {
+  $homeDirectory = getenv('HOME');
+  if (empty($homeDirectory)) {
+    $homeDirectory = getenv("HOMEDRIVE") . getenv("HOMEPATH");
+  }
+  return str_replace('~', realpath($homeDirectory), $path);
+}
+
+function deleteOldest(Google_Service_Drive $service){
+  $files = $service->files->listFiles(array('orderBy'=>'createdTime asc'));
+  echo 'removed '.$files->getFiles()[0]['name'].' (id='.$files->getFiles()[0]['id'].")\n";
+  $service->files->delete($files->getFiles()[0]['id']);
+}
+
+/**
+ * Get the folder ID if it exists, if it doesnt exist, create it and return the ID
+ *
+ * @param Google_Service_Drive $service Drive API service instance.
+ * @param String $folderName Name of the folder you want to search or create
+ * @param String $parentFolderId
+ * @return string id that was created or got. Returns NULL if an API error occured
+ */
+function getFolderExistsCreate($service, $folderName, $parentFolderId=null) {
+  $folderDesc = $folderName;
+  // List all user files (and folders) at Drive root
+  $files = $service->files->listFiles();
+  $found = false;
+  // Go through each one to see if there is already a folder with the specified name
+  foreach ($files['items'] as $item) {
+    if ($item['title'] == $folderName) {
+      $found = true;
+      return $item['id'];
+      break;
+    }
+  }
+  // If not, create one
+  if ($found == false) {
+    $folder = new Google_Service_Drive_DriveFile();
+    //Setup the folder to create
+    $folder->setName($folderName);
+    if(!empty($folderDesc)) $folder->setDescription($folderDesc);
+    $folder->setMimeType('application/vnd.google-apps.folder');
+    //Create the Folder
+    try {
+      $params = array(
+          'mimeType' => 'application/vnd.google-apps.folder',
+      );
+      if($parentFolderId !== null) {
+        $params['parents'] = array($parentFolderId);
+      }
+      $createdFile = $service->files->create($folder, $params);
+      // Return the created folder's id
+      return $createdFile->id;
+    } catch (Exception $e) {
+      print "An error occurred: " . $e->getMessage();
+    }
+  }
+}
+/**
+ * Insert new file in the Application Data folder.
+ *
+ * @param Google_Service_Drive $service Drive API service instance.
+ * @param string $title Title of the file to insert, including the extension.
+ * @param string $description Description of the file to insert.
+ * @param string $mimeType MIME type of the file to insert.
+ * @param string $filename Filename of the file to insert.
+ * @return Google_Service_Drive_DriveFile The file that was inserted. NULL is returned if an API error occurred.
+ */
+function insertFile($service, $title, $description, $mimeType, $filename, $folderName, $folderDesc) {
+  $file = new Google_Service_Drive_DriveFile();
+  // Set the metadata
+  $file->setName($title);
+  $file->setDescription($description);
+  $file->setMimeType($mimeType);
+  // Setup the folder you want the file in, if it is wanted in a folder
+  if(isset($folderName)) {
+    if(!empty($folderName)) {
+      $parent = new Google_Service_Drive_DriveFile();
+      $parent->setId(getFolderExistsCreate($service, $folderName));
+      $file->setParents(array($parent));
+    }
+  }
+  try {
+    // Get the contents of the file uploaded
+    $data = file_get_contents($filename);
+    // Try to upload the file, you can add the parameters e.g. if you want to convert a .doc to editable google format, add 'convert' = 'true'
+    $createdFile = $service->files->create($file, array(
+        'data' => $data,
+        'mimeType' => $mimeType,
+        'uploadType'=> 'multipart'
+    ));
+    // Return a bunch of data including the link to the file we just uploaded
+    return $createdFile;
+  } catch (Exception $e) {
+    print "An error occurred: " . $e->getMessage();
+  }
+}
+
+?>
