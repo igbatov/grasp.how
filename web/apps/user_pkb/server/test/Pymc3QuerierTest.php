b679a93e93a88cb84317941b343963ca28b4418c
Igor Batov <igbatov@gmail.com>
2018-03-16 09:23:10 +0300

    fixed Pymc3QuerierTest

---
+++ web/apps/user_pkb/server/test/Pymc3QuerierTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igbatov
+ * Date: 16-Mar-18
+ * Time: 8:22 AM
+ * Install by downloading phpunit.phar https://phpunit.de/manual/4.8/en/installation.html
+ * Run by
+ * #phpunit Pymc3QuerierTest.php
+ */
+$path = dirname(__FILE__);
+require_once ($path.'/../../../../../web/lib/server/Config.php');
+include_once('../Pymc3Querier.php');
+
+class Pymc3QuerierTest extends PHPUnit_Framework_TestCase
+{
+  /** @var  Pymc3Querier */
+  private $pymc3_querier;
+
+  protected function setUp()
+  {
+    $c = new Config();
+    $this->pymc3_querier = new Pymc3Querier($c->getPymc3Path(), $c->getDefaultPath('tmp'));
+  }
+
+  public function testSimple()
+  {
+    /**
+     * GRAPH 1:
+     *    e2 --> h1 --> e1
+     */
+    $nodes = [
+        'e1'=>[1,2],
+        'e2'=>[1,2],
+        'h1'=>[1,2]
+    ];
+    $edges = [
+        ["e2","h1"],
+        ["h1","e1"]
+    ];
+
+    $graph = ['nodes'=>$nodes, 'edges'=>$edges];
+
+    $probabilities = [
+        'e1'=>[
+            'soft'=>['1'=>0.9, '2'=>0.1], // soft evidence for e1 and ^e1
+            '{"h1":"1"}'=>[1=>0.001, 2=>0.999], // sum must be equal to 1
+            '{"h1":"2"}'=>[1=>0.999, 2=>0.001]  // sum must be equal to 1
+        ],
+        'e2'=>[
+            'soft'=>['1'=>1, '2'=>0], // soft evidence for e2 and ^e2
+        ],
+        'h1'=>[
+            '{"e2":"1"}'=>[1=>0.9999, 2=>0.0001],
+            '{"e2":"2"}'=>[1=>0.5, 2=>0.5]
+        ]
+    ];
+
+    $this->pymc3_querier->initEdgeHashes($graph);
+    $probs = $this->pymc3_querier->createProbabilitiesPart($graph, $probabilities);
+    $mustBe = <<<EOT
+  e1_prob = np.array([0.5, 0.5])  
+  e1_virtual_prob = np.array([
+    [0.9, 0.1],
+    [0.1, 0.9]
+  ])
+  e1_prob = np.array([
+    [0.001, 0.999],
+    [0.999, 0.001]
+  ])
+  e2_prob = np.array([0.5, 0.5])
+  h1_prob = np.array([
+    [0.9999, 0.0001],
+    [0.5, 0.5]
+  ])
+EOT;
+
+    $this->assertMultiLineEquals($mustBe, $probs);
+
+  }
+
+  private function assertMultiLineEquals($mustBe, $result){
+    $mustBeLines = explode("\n", $mustBe);
+    $resultLines = explode("\n",$result);
+    foreach ($mustBeLines as $i => $mustBeLine) {
+      $this->assertEquals(trim($mustBeLine, "\n\r"), trim($resultLines[$i],"\n\r"));
+    }
+  }
+}
