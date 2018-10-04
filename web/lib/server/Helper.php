907d394125d4dd009f0e87ea8c10d6e40e536a69
Igor <igbatov@gmail.com>
2018-10-04 22:03:58 +0300

    fixed bin/node path

--- web/lib/server/Helper.php
+++ web/lib/server/Helper.php
@@ -12,7 +12,7 @@ class Helper {
 
   public function getNodeBinary(){
     if ($this->getOS() === 'linux') {
-      return '/root/.nvm/versions/node/v4.4.3/bin/node';
+      return '/usr/bin/node';
     }
 
     if ($this->getOS() === 'windows') {
