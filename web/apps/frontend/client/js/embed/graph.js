6cd857f268584e7cedbffd88a67a3f9c54e64f0d
Igor Batov <igbatov@gmail.com>
2018-11-21 19:55:09 +0100

    fixed graph.js for image generator

--- web/apps/frontend/client/js/embed/graph.js
+++ web/apps/frontend/client/js/embed/graph.js
@@ -1,3 +1,20 @@
+/** Common to nodejs and web helpers */
+var GRASP = GRASP || {};
+
+GRASP.html2text = function(html){
+  html = html.replace(/<style([\s\S]*?)<\/style>/gi, '');
+  html = html.replace(/<script([\s\S]*?)<\/script>/gi, '');
+  html = html.replace(/<div>/ig, '\n');
+  html = html.replace(/<\/div>/ig, '\n');
+  html = html.replace(/<\/li>/ig, '\n');
+  html = html.replace(/<li>/ig, '  *  ');
+  html = html.replace(/<\/ul>/ig, '\n');
+  html = html.replace(/<\/p>/ig, '\n');
+  html = html.replace(/<br\s*[\/]?>/gi, "\n");
+  html = html.replace(/<[^>]+>/ig, '');
+  html = html.replace(/&nbsp;/ig, '');
+  return html;
+}
 
 /**
  * Draws SVG given graph nodes, edges, labels
