ebf334f38136e66e0070a95e2ff8f1966e4ee8d4
Igor <igbatov@gmail.com>
2019-03-19 22:49:20 +0300

    removed yandex webvisor in lk

--- web/apps/user_pkb/client/template/index.php
+++ web/apps/user_pkb/client/template/index.php
@@ -53,22 +53,5 @@
       document.getElementsByTagName("head")[0].appendChild(mf);
     })();
   </script>
-
-  <!-- yandex webvisor -->
-  <!-- Yandex.Metrika counter -->
-  <script type="text/javascript" >
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-      m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-    ym(52877368, "init", {
-      clickmap:false,
-      trackLinks:false,
-      accurateTrackBounce:false,
-      webvisor:true
-    });
-  </script>
-  <noscript><div><img src="https://mc.yandex.ru/watch/52877368" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-  <!-- /Yandex.Metrika counter -->
   </body>
 </html>
