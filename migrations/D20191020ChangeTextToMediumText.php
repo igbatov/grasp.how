035dc937ef7c2bd5d8585104ee3cd92bf6a4f584
Igor <igbatov@gmail.com>
2019-10-20 23:32:53 +0300

    fixed migration for text field type change to mediumtext

--- migrations/D20191020ChangeTextToMediumText.php
+++ migrations/D20191020ChangeTextToMediumText.php
@@ -3,25 +3,47 @@
 class D20191020ChangeTextToMediumText extends Migration {
   public function up($authId)
   {
-    $this->db->exec(
-      $authId,
-      'alter table node_content modify `text` mediumtext null;'
-    );
-    $this->db->exec(
-      $authId,
-      'alter table node_content_history modify `text` mediumtext null;'
-    );
+    if ($authId === NULL) {
+      $this->db->exec(
+        $authId,
+        'alter table yovalue_user_template.node_content modify `text` mediumtext null;'
+      );
+      $this->db->exec(
+        $authId,
+        'alter table yovalue_user_template.node_content_history modify `text` mediumtext null;'
+      );
+    } else {
+      $this->db->exec(
+        $authId,
+        'alter table node_content modify `text` mediumtext null;'
+      );
+      $this->db->exec(
+        $authId,
+        'alter table node_content_history modify `text` mediumtext null;'
+      );
+    }
   }
 
   public function down($authId)
   {
-    $this->db->exec(
-      $authId,
-      'alter table node_content modify `text` text null;'
-    );
-    $this->db->exec(
-      $authId,
-      'alter table node_content_history modify `text` text null;'
-    );
+    if ($authId === NULL) {
+      $this->db->exec(
+        $authId,
+        'alter table yovalue_user_template.node_content modify `text` text null;'
+      );
+      $this->db->exec(
+        $authId,
+        'alter table yovalue_user_template.node_content_history modify `text` text null;'
+      );
+    } else {
+      $this->db->exec(
+        $authId,
+        'alter table node_content modify `text` text null;'
+      );
+      $this->db->exec(
+        $authId,
+        'alter table node_content_history modify `text` text null;'
+      );
+    }
   }
 }
